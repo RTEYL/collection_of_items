b56c8b3968d01f7610a680e8ba2a84d68def64bf
RTEYL <tyler-w-jones@hotmail.com>
2020-05-20 20:28:15 -0700

    set up validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :collections
   has_many :items, through: :collections
   has_secure_password
-  validates :name, :password_confirmation, presence: true, length: {minimum: 3, maximum: 12}
-  validates :password, confirmation: true, length: {minimum: 3, maximum: 12}
+  validates :username, :password_confirmation, presence: true
+  validates :password, confirmation: true
 end
