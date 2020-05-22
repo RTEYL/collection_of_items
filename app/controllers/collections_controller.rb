65d015a9cce0619af863c8d1e081504c182d0933
RTEYL <tyler-w-jones@hotmail.com>
2020-05-22 15:11:06 -0700

    setup collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,27 +2,51 @@ class CollectionsController < ApplicationController
 
   # GET: /collections
   get "/collections" do
-    erb :"/collections/index.html"
+    if logged_in?
+      erb :"/collections/index.html"
+    else
+      flash[:message] = 'You must be logged in to use that feature'
+      redirect '/users/login'
+    end
   end
 
   # GET: /collections/new
   get "/collections/new" do
-    erb :"/collections/new.html"
+    if logged_in? && current_user
+      erb :"/collections/new.html"
+    else
+      flash[:message] = 'You must be logged in to use that feature'
+      redirect '/users/login'
+    end
   end
 
   # POST: /collections
   post "/collections" do
-    redirect "/collections"
+    user = User.find(session["user_id"])
+    params.delete_if{|p| p == "submit"}
+    params[:user_id] = user.id
+    Collection.create(params)
+    redirect "/users/#{user.id}"
   end
 
   # GET: /collections/5
   get "/collections/:id" do
-    erb :"/collections/show.html"
+    if logged_in? && current_user
+      erb :"/collections/show.html"
+    else
+      flash[:message] = 'You must be logged in to use that feature'
+      redirect '/users/login'
+    end
   end
 
   # GET: /collections/5/edit
   get "/collections/:id/edit" do
-    erb :"/collections/edit.html"
+    if logged_in? && current_user
+      erb :"/collections/edit.html"
+    else
+      flash[:message] = 'You must be logged in to use that feature'
+      redirect '/users/login'
+    end
   end
 
   # PATCH: /collections/5
