e06c475b20058a8e888d2f68e293386112255916
RTEYL <tyler-w-jones@hotmail.com>
2020-05-21 20:13:26 -0700

    extend functionality of validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   has_many :collections
   has_many :items, through: :collections
   has_secure_password
-  validates :username, :password_confirmation, presence: true
-  validates :password, confirmation: true
+  validates :username, :password_confirmation, presence: true, if: :has_password?
+  validates :password, confirmation: true, if: :has_password?
+
+  def has_password?
+    false if !self.password
+  end
 end
