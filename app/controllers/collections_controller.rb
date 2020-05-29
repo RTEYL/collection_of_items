1b5c24c2158da62fbeb0448d4d49dede95e1cacc
RTEYL <tyler-w-jones@hotmail.com>
2020-05-28 17:58:37 -0700

    fix image url validations

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -27,14 +27,16 @@ class CollectionsController < ApplicationController
     params.delete_if{|p| p == "submit"}
     col = Collection.new(name: params[:collection][:name], description: params[:collection][:description])
     params["collection"]["items"].each do |item|
-      new_item = Item.new(name: item["name"], condition: item["condition"], img_url: item["img_url"]) if item["name"] != ""
-      if new_item.valid?
-        new_item.collection = col
-        col.items << new_item
-        new_item.save
-      else
-        flash[:message] = "Invalid Image Url"
-        redirect "/collections/new"
+      if item["name"] != ""
+        new_item = Item.new(name: item["name"], condition: item["condition"], img_url: item["img_url"])
+        if new_item.valid?
+          new_item.collection = col
+          new_item.save
+          col.items << new_item
+        else
+          flash[:message] = "Invalid Image Url"
+          redirect "/collections/new"
+        end
       end
     end
     if col.valid?
@@ -50,6 +52,7 @@ class CollectionsController < ApplicationController
   # GET: /collections/5
   get "/collections/:id" do
     if accessible?
+
       @collection = Collection.find(params[:id])
       @items = @collection.items
       @user = @collection.user
@@ -63,6 +66,7 @@ class CollectionsController < ApplicationController
   # GET: /collections/5/edit
   get "/collections/:id/edit" do
     if accessible?
+      @collection = Collection.find_by_id(params[:id])
       erb :"/collections/edit.html"
     else
       flash[:message] = 'You must be logged in to use that feature'
@@ -72,7 +76,25 @@ class CollectionsController < ApplicationController
 
   # PATCH: /collections/5
   patch "/collections/:id" do
-    redirect "/collections/:id"
+    col = Collection.find(params[:id])
+    col.update(name: params[:collection][:name], description: params[:collection][:description], item_ids: params[:collection][:item_ids])
+    if params[:collection][:items]["name"] != ""
+      item = Item.new(
+        name: params[:collection][:items]["name"],
+        condition: params[:collection][:items]["condition"],
+        img_url: params[:collection][:items]["img_url"]
+      )
+      if item.valid?
+        item.collection = col
+        item.save
+        col.items << item
+      else
+        flash[:message] = item.error.messages
+        redirect "/collections/#{col.id}"
+      end
+    end
+    col.save if col.valid?
+    redirect "/collections/#{col.id}}"
   end
 
   # DELETE: /collections/5/delete
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :collection
-  validates :img_url, format: {with: /\.(png|jpg)\Z/i}
+  validates :img_url, allow_blank: true, format: {
+    with: %r{\.gif|jpg|png|jpeg}i,
+    message: 'must be a url for gif, jpg, or png image.'
+  }
 end
